1d6163ea5ba958b5be852a02231baeac50e025da
venky422 <dommaravenkatesulu22@gmail.com>
2022-05-27 23:24:33 +0530

    completed

---
+++ forgotpassword.js
@@ -0,0 +1,31 @@
+const forgot=async()=>{
+    
+    let inputValue=document.getElementById("forgot_password_request").value;
+    if(inputValue.includes("@")){
+        try {
+            var res = await fetch(`http://localhost:3005/users?email=${inputValue}`);
+            var data=await res.json();
+            var p=data[0].password;
+            alert("Your password is:"+p);
+            window.location.href="./login.html"
+        } catch(error) {
+            document.querySelector("#error_text").innerHTML="*please enter valid email or your mobile number";
+        }
+    }
+    else{
+        try {
+        var res =fetch(`http://localhost:3005/users?number=${inputValue}`);
+
+            var data= await res.json();
+            var p=data[0].password;
+            alert("Your password is:"+p);
+            window.location.href="./login.html"
+        } catch (error) {
+        
+        document.querySelector("#error_text").innerHTML="*please enter valid email or your mobile number";   }
+    }
+
+
+}
+
+export {forgot} 
